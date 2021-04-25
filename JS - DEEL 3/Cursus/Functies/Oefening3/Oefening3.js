5f701a95968dcc03d1f82b0f7003ba9c8d0cbf6f
Wolooo <agnaouilias@hotmail.com>
2021-04-25 17:48:54 +0200

    oefening3 van functies

--- JS - DEEL 3/Cursus/Functies/Oefening3/Oefening3.js
+++ JS - DEEL 3/Cursus/Functies/Oefening3/Oefening3.js
@@ -2,7 +2,7 @@ function IsVijftig(getal1, getal2) {
     if ((getal1 === 50 || getal2 === 50) || (getal1 + getal2 === 50)) {
         return "HOERA 50";
     } else {
-        return "Helaas geen 54";
+        return "Helaas geen 50";
     }
 }
 
